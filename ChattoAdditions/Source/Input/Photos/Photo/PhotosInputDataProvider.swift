fff5eaf3fef41109c47de95f8975e701c55936bf
Qin Ling <qinling97@qq.com>
2018-11-20 14:12:10 +0800

    try to filter out iCloud medias

--- ChattoAdditions/Source/Input/Photos/Photo/PhotosInputDataProvider.swift
+++ ChattoAdditions/Source/Input/Photos/Photo/PhotosInputDataProvider.swift
@@ -68,6 +68,7 @@ final class PhotosInputDataProvider: NSObject, PhotosInputDataProviderProtocol, 
             let options = PHFetchOptions()
             options.sortDescriptors = [ NSSortDescriptor(key: "creationDate", ascending: false) ]
             options.predicate = predicate
+            options.includeAssetSourceTypes = PHAssetSourceTypeUserLibrary
             return options
         }
 
